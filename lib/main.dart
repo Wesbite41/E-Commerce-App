2b2c27c549dcb5ef048ec9ab9f66b4ace40fcdb6
Canbozyigitt <136346490+Canbozyigitt@users.noreply.github.com>
2024-04-19 17:04:24 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wesbite Demo',
+      title: 'can Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
